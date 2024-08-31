292684bd17c0d410eba1fa08067acd07614a34ca
osos11 <80286641+osos11-Git@users.noreply.github.com>
2024-08-31 22:46:47 +0300

    Enhanced SBUS Decoding

--- F401_RadioLink_SBUS/Core/Src/main.c
+++ F401_RadioLink_SBUS/Core/Src/main.c
@@ -26,6 +26,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <string.h>
+#include <stdbool.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -59,57 +60,94 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 uint16_t failsafe_status;
+uint16_t SBUS_footer;
 uint8_t buf[25];
 uint16_t CH[18];
-int lenght=0;
-uint16_t USB_Send_Data[]={0};
+
+uint8_t pre_catch_buf[1];
+int pre_init_counter = 0;
+bool start_counting = false;
+bool catch_signal = false;
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
+	if (pre_catch_buf[0] == 0x0F) start_counting = true;
 
-	if (buf[0] == 0x0F) {
-		CH[0] = (buf[1] >> 0 | (buf[2] << 8)) & 0x07FF;
-		CH[1] = (buf[2] >> 3 | (buf[3] << 5)) & 0x07FF;
-		CH[2] = (buf[3] >> 6 | (buf[4] << 2) | buf[5] << 10) & 0x07FF;
-		CH[3] = (buf[5] >> 1 | (buf[6] << 7)) & 0x07FF;
-		CH[4] = (buf[6] >> 4 | (buf[7] << 4)) & 0x07FF;
-		CH[5] = (buf[7] >> 7 | (buf[8] << 1) | buf[9] << 9) & 0x07FF;
-		CH[6] = (buf[9] >> 2 | (buf[10] << 6)) & 0x07FF;
-		CH[7] = (buf[10] >> 5 | (buf[11] << 3)) & 0x07FF;
-		CH[8] = (buf[12] << 0 | (buf[13] << 8)) & 0x07FF;
-		CH[9] = (buf[13] >> 3 | (buf[14] << 5)) & 0x07FF;
-		CH[10] = (buf[14] >> 6 | (buf[15] << 2) | buf[16] << 10) & 0x07FF;
-		CH[11] = (buf[16] >> 1 | (buf[17] << 7)) & 0x07FF;
-		CH[12] = (buf[17] >> 4 | (buf[18] << 4)) & 0x07FF;
-		CH[13] = (buf[18] >> 7 | (buf[19] << 1) | buf[20] << 9) & 0x07FF;
-		CH[14] = (buf[20] >> 2 | (buf[21] << 6)) & 0x07FF;
-		CH[15] = (buf[21] >> 5 | (buf[22] << 3)) & 0x07FF;
-
-		if (buf[23] & (1 << 0)) {
-			CH[16] = 1;
-		} else {
-			CH[16] = 0;
+	if (pre_init_counter < 24 && start_counting == true)
+		{
+			pre_init_counter++;
+			return;
+		}
+	else if (catch_signal == false)
+		{
+			catch_signal = true;
+			HAL_UART_AbortReceive(&huart2);
+			HAL_UART_Receive_DMA(&huart2, buf, 25);
+			return;
 		}
 
-		if (buf[23] & (1 << 1)) {
-			CH[17] = 1;
-		} else {
-			CH[17] = 0;
+	if (buf[0] == 0x0F)
+		{
+			CH[0] = (buf[1] >> 0 | (buf[2] << 8)) & 0x07FF;
+			CH[1] = (buf[2] >> 3 | (buf[3] << 5)) & 0x07FF;
+			CH[2] = (buf[3] >> 6 | (buf[4] << 2) | buf[5] << 10) & 0x07FF;
+			CH[3] = (buf[5] >> 1 | (buf[6] << 7)) & 0x07FF;
+			CH[4] = (buf[6] >> 4 | (buf[7] << 4)) & 0x07FF;
+			CH[5] = (buf[7] >> 7 | (buf[8] << 1) | buf[9] << 9) & 0x07FF;
+			CH[6] = (buf[9] >> 2 | (buf[10] << 6)) & 0x07FF;
+			CH[7] = (buf[10] >> 5 | (buf[11] << 3)) & 0x07FF;
+			CH[8] = (buf[12] << 0 | (buf[13] << 8)) & 0x07FF;
+			CH[9] = (buf[13] >> 3 | (buf[14] << 5)) & 0x07FF;
+			CH[10] = (buf[14] >> 6 | (buf[15] << 2) | buf[16] << 10) & 0x07FF;
+			CH[11] = (buf[16] >> 1 | (buf[17] << 7)) & 0x07FF;
+			CH[12] = (buf[17] >> 4 | (buf[18] << 4)) & 0x07FF;
+			CH[13] = (buf[18] >> 7 | (buf[19] << 1) | buf[20] << 9) & 0x07FF;
+			CH[14] = (buf[20] >> 2 | (buf[21] << 6)) & 0x07FF;
+			CH[15] = (buf[21] >> 5 | (buf[22] << 3)) & 0x07FF;
+
+			if (buf[23] & (1 << 0))
+				{
+					CH[16] = 1;
+				}
+			else
+				{
+					CH[16] = 0;
+				}
+
+			if (buf[23] & (1 << 1))
+				{
+					CH[17] = 1;
+				}
+			else
+				{
+					CH[17] = 0;
+				}
+
+			// Failsafe
+			failsafe_status = SBUS_SIGNAL_OK;
+			if (buf[23] & (1 << 2))
+				{
+					failsafe_status = SBUS_SIGNAL_LOST;
+				}
+
+			if (buf[23] & (1 << 3))
+				{
+					failsafe_status = SBUS_SIGNAL_FAILSAFE;
+				}
+
+			SBUS_footer = buf[24];
+
 		}
-
-		// Failsafe
-		failsafe_status = SBUS_SIGNAL_OK;
-		if (buf[23] & (1 << 2)) {
-			failsafe_status = SBUS_SIGNAL_LOST;
+	else
+		{
+			catch_signal = false;
+			start_counting = false;
+			catch_signal = false;
+			pre_init_counter = 0;
+			HAL_UART_AbortReceive(&huart2);
+			HAL_UART_Receive_DMA(&huart2, pre_catch_buf, 1);
+			return;
 		}
-
-		if (buf[23] & (1 << 3)) {
-			failsafe_status = SBUS_SIGNAL_FAILSAFE;
-		}
-
-		//	SBUS_footer=buf[24];
-
-	}
 }
 /* USER CODE END 0 */
 
@@ -144,7 +182,7 @@ int main(void)
   MX_DMA_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  HAL_UART_Receive_DMA(&huart2, buf, 25);
+  HAL_UART_Receive_DMA(&huart2, pre_catch_buf, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
